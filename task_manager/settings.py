568df4eabfcfc21a8ef770ff8fc46742a7f2bbae
Mirshod2006 <xamrayevmironshoh4@gmail.com>
2026-06-16 17:49:08 +0500

    Git ignore has been added and port number for database has been changed

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "1") in ("1", "true", "True", "yes")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-
+#ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS=['*']
 
 # Application definition
 
